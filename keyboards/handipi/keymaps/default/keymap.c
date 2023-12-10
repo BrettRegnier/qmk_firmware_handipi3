4e68cc131b04abd81876b30cfaa3e66071f373e4
Brett Regnier <bretternestregnier@gmail.com>
2023-12-09 17:17:58 -0700

    Add turning off of autoshift

--- keyboards/handipi/keymaps/default/keymap.c
+++ keyboards/handipi/keymaps/default/keymap.c
@@ -7,7 +7,8 @@
 
 #define LAYER_TOGGLE_CHORD KC_ENT
 #define CONTROL_CHORD KC_SPC
-#define ALT_CHORD KC_SCLN
+#define ALT_CHORD KC_ESC
+#define AM_TOGGLE KC_TAB
 
 // void CheckForBootloader(void);
 // void LayerUp(int* next_layer, const int num_layers);
@@ -15,34 +16,62 @@
 
 
 // Global variables
+bool mouse=false;
+bool autoshift_on=true;
+
 
 enum
 {
     TD_MOUSE_CLICK = 0,
-    TD_L_S_BRACKET,
-    TD_R_S_BRACKET,
-    TD_L_C_BRACKET,
-    TD_R_C_BRACKET,
-    TD_PLUS,
-    TD_MINUS,
-    TD_UNDER,
-    TD_EQUAL,
-    TD_TILDE
+    TD_ALT_F1,
+    TD_ALT_F2,
+    TD_ALT_F3,
+    TD_ALT_F4,
+    TD_ALT_F5,
+    TD_ALT_F6,
+    TD_ALT_F7,
+    TD_ALT_F8,
+    TD_ALT_F9
+};
+
+enum 
+{
+    LAYER_SHIFT = SAFE_RANGE, 
+    NOM_F1,
+    NOM_F2,
+    NOM_F3,
+    NOM_F4,
+    NOM_F5,
+    NOM_F6,
+    NOM_F7,
+    NOM_F8,
+    NOM_F9,
+    ALT_F1,
+    ALT_F2,
+    ALT_F3,
+    ALT_F4,
+    ALT_F5,
+    ALT_F6,
+    ALT_F7,
+    ALT_F8,
+    ALT_F9,
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions [] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_MOUSE_CLICK] = ACTION_TAP_DANCE_DOUBLE(KC_BTN1, KC_BTN2),
-    [TD_L_S_BRACKET] = ACTION_TAP_DANCE_DOUBLE(KC_O, KC_LEFT_BRACKET),
-    [TD_R_S_BRACKET] = ACTION_TAP_DANCE_DOUBLE(KC_P, KC_RIGHT_BRACKET),
-    [TD_L_C_BRACKET] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_LEFT_CURLY_BRACE),
-    [TD_R_C_BRACKET] = ACTION_TAP_DANCE_DOUBLE(KC_W, KC_RIGHT_CURLY_BRACE),
-    [TD_PLUS] = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_PLUS),
-    [TD_MINUS] = ACTION_TAP_DANCE_DOUBLE(KC_J, KC_MINUS),
-    [TD_UNDER] = ACTION_TAP_DANCE_DOUBLE(KC_K, KC_UNDERSCORE),
-    [TD_EQUAL] = ACTION_TAP_DANCE_DOUBLE(KC_L, KC_EQUAL),
-    [TD_TILDE] = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, KC_GRAVE),
+    //[TD_ALT_F1] = ACTION_TAP_DANCE_DOUBLE(KC_F1, SS_LALT(KC_F1)),
+    //[TD_ALT_F2] = ACTION_TAP_DANCE_DOUBLE(KC_F2, SS_LALT(KC_F2)),
+    [TD_ALT_F1] = ACTION_TAP_DANCE_DOUBLE(NOM_F1, ALT_F1),
+    [TD_ALT_F2] = ACTION_TAP_DANCE_DOUBLE(NOM_F2, ALT_F2),
+    [TD_ALT_F3] = ACTION_TAP_DANCE_DOUBLE(NOM_F3, ALT_F3),
+    [TD_ALT_F4] = ACTION_TAP_DANCE_DOUBLE(NOM_F4, ALT_F4),
+    [TD_ALT_F5] = ACTION_TAP_DANCE_DOUBLE(NOM_F5, ALT_F5),
+    [TD_ALT_F6] = ACTION_TAP_DANCE_DOUBLE(NOM_F6, ALT_F6),
+    [TD_ALT_F7] = ACTION_TAP_DANCE_DOUBLE(NOM_F7, ALT_F7),
+    [TD_ALT_F8] = ACTION_TAP_DANCE_DOUBLE(NOM_F8, ALT_F8),
+    [TD_ALT_F9] = ACTION_TAP_DANCE_DOUBLE(NOM_F9, ALT_F9),
 };
 
 const uint16_t PROGMEM keymaps [][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,7 +88,7 @@ const uint16_t PROGMEM keymaps [][MATRIX_ROWS][MATRIX_COLS] = {
      *                     ┌───┐
      *                     │UPA│
      *                 ┌───┼───┼───┐
-     *                 │LFA│CLK│RTA│
+     *                 │LFA│TOG│RTA│
      *                 └───├───┼───┘
      *                     │DNA│
      *                     └───┘
@@ -70,64 +99,105 @@ const uint16_t PROGMEM keymaps [][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_A,   KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,     KC_L,   KC_QUOTE},
         {KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT, KC_SLASH},
         {KC_NO,  KC_NO,  KC_SCLN, KC_SPC,  KC_TAB,  KC_ENT,  KC_BSPC,  KC_DEL,   KC_NO,  KC_NO},
-        {KC_NO,  KC_NO,  KC_DOWN, KC_LEFT, KC_UP,   TD(TD_MOUSE_CLICK), KC_RIGHT, KC_NO,    KC_NO,  KC_NO},
+        {KC_NO,  KC_NO,  KC_DOWN, KC_LEFT, KC_UP,   LAYER_SHIFT, KC_RIGHT, KC_NO,    KC_NO,  KC_NO},
     },
 
     /*
      * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
      * │ 1 │ 2 │ 3 │ 4 │ 5 │   │ 6 │ 7 │ 8 │ 9 │ 0 │
      * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
-     * │ [ │ ] │ { │ } │ G │   │ + │ - │ _ │ = │ ` │
+     * │ [ │ ] │NOP│NOP│NOP│   │ = │ - │NOP│NOP│ ` │
      * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
-     * │ Z │ X │ C │ V │ B │   │ N │ M │ < │ > │ \ │
+     * │TF1│TF2│TF3│TF4│TF5│   │TF6│TF7│TF8│TF9│ \ │
      * └───┴───├───┼───┼───┤   ├───├───┼───┼───┴───┘
-     *         │ ; │spc│tab│   │ent│ctl│alt│
+     *         │esc│spc│tab│   │ent│ctl│alt│
      *         └───┴───┴───┘   └───┴───┴───┘
      *                     ┌───┐
-     *                     │MSU│
+     *                     │UPA│
      *                 ┌───┼───┼───┐
-     *                 │MSL│CLK│MSR│
+     *                 │LFA│TOG│RTA│
      *                 └───├───┼───┘
-     *                     │MSD│
+     *                     │DNA│
      *                     └───┘
      */
 
     [1] = {
         {KC_1,   KC_2,   KC_3,    KC_4,    KC_5,   KC_6,    KC_7,     KC_8,    KC_9,   KC_0},
-        {KC_LEFT_BRACKET,   KC_RIGHT_BRACKET,   KC_LEFT_CURLY_BRACE,    KC_RIGHT_CURLY_BRACE,    KC_NO,   KC_PLUS,    KC_MINUS,     KC_UNDERSCORE,    KC_EQUAL,   KC_GRAVE},
-        {KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,   KC_N,    KC_M,     KC_LABK, KC_RABK, KC_BACKSLASH},
+        {KC_LEFT_BRACKET,   KC_RIGHT_BRACKET,   KC_NO,    KC_NO,    KC_NO,   KC_EQUAL,    KC_MINUS,     KC_NO,    KC_NO,   KC_GRAVE},
+        {KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,    KC_F7,     KC_F8, KC_F9, KC_BACKSLASH},
         {KC_NO,  KC_NO,  KC_ESC,  KC_SPC,  KC_TAB, KC_ENT,  KC_BSPC,  KC_LALT,  KC_NO,  KC_NO},
-        {KC_NO,  KC_NO,  KC_MS_DOWN, KC_MS_LEFT, KC_MS_UP,  TD(TD_MOUSE_CLICK), KC_MS_RIGHT, KC_NO,   KC_NO,  KC_NO},
+        {KC_NO,  KC_NO,  KC_DOWN, KC_LEFT, KC_UP,  LAYER_SHIFT, KC_RIGHT, KC_NO,   KC_NO,  KC_NO},
+    },
+
+    /*
+     * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
+     * │ Q │ W │ E │ R │ T │   │ Y │ U │ I │ O │ P │
+     * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
+     * │ A │ S │ D │ F │ G │   │ H │ J │ K │ L │ ' │
+     * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
+     * │ Z │ X │ C │ V │ B │   │ N │ M │ , │ . │ ? │
+     * └───┴───├───┼───┼───┤   ├───├───┼───┼───┴───┘
+     *         │ ; │spc│tab│   │ent│bsp│del│
+     *         └───┴───┴───┘   └───┴───┴───┘
+     *                     ┌───┐
+     *                     │UPA│
+     *                 ┌───┼───┼───┐
+     *                 │LFA│TOG│RTA│
+     *                 └───├───┼───┘
+     *                     │DNA│
+     *                     └───┘
+     */
+
+    [2] = {
+        {KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,     KC_O,   KC_P},
+        {KC_A,   KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,     KC_L,   KC_QUOTE},
+        {KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT, KC_SLASH},
+        {KC_NO,  KC_NO,  KC_SCLN, KC_SPC,  KC_TAB,  KC_ENT,  KC_BSPC,  KC_DEL,   KC_NO,  KC_NO},
+        {KC_NO,  KC_NO,  KC_DOWN, KC_LEFT, KC_UP,   LAYER_SHIFT, KC_RIGHT, KC_NO,    KC_NO,  KC_NO},
     },
 
     /*
      * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
      * │ 1 │ 2 │ 3 │ 4 │ 5 │   │ 6 │ 7 │ 8 │ 9 │ 0 │
      * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
-     * │ [ │ ] │ { │ } │ G │   │ + │ - │ _ │ = │ ` │
+     * │ [ │ ] │NOP│NOP│ G │   │ = │ - │NOP│NOP│ ` │
      * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
      * │ Z │ X │ C │ V │ B │   │ N │ M │ < │ > │ \ │
      * └───┴───├───┼───┼───┤   ├───├───┼───┼───┴───┘
      *         │ ; │spc│tab│   │ent│ctl│alt│
      *         └───┴───┴───┘   └───┴───┴───┘
      *                     ┌───┐
-     *                     │MSU│
+     *                     │UPA│
      *                 ┌───┼───┼───┐
-     *                 │MSL│CLK│MSR│
+     *                 │LFA│NOP│RTA│
      *                 └───├───┼───┘
-     *                     │MSD│
+     *                     │DNA│
      *                     └───┘
      */
 
-    [2] = {
+    [3] = {
         {KC_NO,   KC_NO,   KC_NO,      KC_NO,      KC_NO,    KC_NO,  KC_NO,       KC_NO, KC_NO, KC_NO},
         {KC_NO,   KC_NO,   KC_NO,      KC_NO,      KC_NO,    KC_NO,  KC_NO,       KC_NO, KC_NO, KC_NO},
         {KC_NO,   KC_NO,   KC_NO,      KC_NO,      KC_NO,    KC_NO,  KC_NO,       KC_NO, KC_NO, QK_BOOT},
         {KC_NO,   KC_NO,   KC_NO,      KC_NO,      KC_NO,    KC_ENT, KC_NO,       KC_NO, KC_NO, KC_NO},
-        {KC_NO,   KC_NO,   KC_MS_DOWN, KC_MS_LEFT, KC_MS_UP, KC_NO,  KC_MS_RIGHT, KC_NO, KC_NO, KC_NO},
+        {KC_NO,   KC_NO,   KC_DOWN, KC_LEFT, KC_UP, KC_NO,  KC_RIGHT, KC_NO, KC_NO, KC_NO},
     }
 
 };
+
+layer_state_t layer_state_set_user(layer_state_t state)
+{
+    switch (get_highest_layer(state)) 
+    {
+        case 2:
+            autoshift_disable();
+            break;
+        default:
+            autoshift_enable();
+            break;
+    }    
+    return state;
+}
 
 // // Tap Dance function
 // void mouse_click_tap_dance(qk_tap_dance_state_t* state, void* user_data)
@@ -192,7 +262,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record)
     static bool control_chord_active = false;
     static bool alt_chord_active = false;
     static bool did_combo = false;
-
+    static bool layer_shift = false;
 
     // CheckForBootloader();
 
@@ -266,10 +336,71 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record)
 
             return false;
         }
+        case LAYER_SHIFT:
+            if (record->event.pressed && !layer_shift)
+            {
+                layer_shift = true;
+                // Change layer up
+                if (next_layer + 1 != num_layers)
+                {
+                    layer_off(next_layer++);
+                    layer_on(next_layer);
+                }
+            }
+            else if (layer_shift)
+            {
+                layer_shift = false;
+                // Change layer down
+                if (next_layer > 0)
+                {
+                    layer_off(next_layer--);
+                    layer_on(next_layer);
+                }
+            }
+            return false;
+            
     }
 
     if (record->event.pressed)
     {
+        switch(keycode)
+        {
+        case ALT_F1:
+            register_code(KC_LALT);
+            register_code(KC_F1);
+            return false;
+        case ALT_F2:
+            tap_code16(QK_LALT | KC_F2);
+            return false;
+        case ALT_F3:
+            unregister_code16(ALT_F3);
+            register_code16(QK_LALT | KC_F3);
+            return false;
+        case ALT_F4:
+            register_code(KC_LALT);
+            register_code(KC_F4);
+            return false;
+        case ALT_F5:
+            register_code(KC_LALT);
+            register_code(KC_F5);
+            return false;
+        case ALT_F6:
+            register_code(KC_LALT);
+            register_code(KC_F6);
+            return false;
+        case ALT_F7:
+            register_code(KC_LALT);
+            register_code(KC_F7);
+            return false;
+        case ALT_F8:
+            register_code(KC_LALT);
+            register_code(KC_F8);
+            return false;
+        case ALT_F9:
+            register_code(KC_LALT);
+            register_code(KC_F9);
+            return false;
+        }
         if (control_chord_active)
         {
             // Send a control chord before the current key?
@@ -291,6 +422,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record)
 
             return false;
         }
+
         if (toggle_chord_active)
         {
             switch (keycode)
@@ -335,6 +467,51 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record)
                     }
                     return false;
                 }
+                case KC_TAB:
+                {
+                    changed_layers=true;
+                    mouse = !mouse;   
+                }
+                case KC_SPC:
+                {
+                    changed_layers=true;
+                    if (autoshift_on)
+                    {
+                        autoshift_disable();
+                        autoshift_on = false;
+                    }
+                    else 
+                    {
+                        autoshift_enable();
+                        autoshift_on = true;
+                    }
+                }
+            }
+        }
+        if (mouse)
+        {
+            switch (keycode)
+            {
+                case KC_UP:
+                    unregister_code(KC_UP);
+                    tap_code(KC_MS_UP);
+                    return false;
+                case KC_DOWN: 
+                    unregister_code(KC_DOWN);
+                    tap_code(KC_MS_DOWN);
+                    return false;
+                case KC_LEFT: 
+                    unregister_code(KC_LEFT);
+                    tap_code(KC_MS_LEFT);
+                    return false;
+                case KC_RIGHT: 
+                    unregister_code(KC_RIGHT);
+                    tap_code(KC_MS_RIGHT);
+                    return false;
+                case LAYER_SHIFT:
+                    unregister_code16(LAYER_SHIFT);
+                    register_code16(TD(TD_MOUSE_CLICK));
+                    return false;
             }
         }
     }
